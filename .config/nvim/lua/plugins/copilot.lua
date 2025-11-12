645c04d03a3affdb6a7db6331a0bac45c5b2b728
Maximilian Barth <maximilian@barth.zip>
2025-11-12 09:34:09 +0100

    lnk: sync configuration files

--- .config/nvim/lua/plugins/copilot.lua
+++ .config/nvim/lua/plugins/copilot.lua
@@ -1,5 +1,6 @@
 return {
   "github/copilot.vim",
+  enabled = false,
   config = function()
     vim.keymap.set("i", "<c-q>", "copilot#Accept('\\<cr>')", { expr = true, silent = true, replace_keycodes = false })
     vim.g.copilot_no_tab_map = true
